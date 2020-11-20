a9638e3047a3c43552d6c574e6fd521b6dd723d5
jantiks <58886410+jantiks@users.noreply.github.com>
2020-11-20 23:26:43 +0400

    controllers are ready

---
+++ presentation/PageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PageViewController.swift
+//  presentation
+//
+//  Created by Tigran on 11/20/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
